650067c66024b6af8e4751009d359daa2b2a776f
cheran <cherankumar.548@gmail.com>
2022-06-05 10:30:34 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("cheran");
+  const [toggle, setToggle] = useState(false);
+  function clickHandler(e) {
+    e.preventDefault();
+    if (!toggle) {
+      setName("changed");
+      setToggle(true);
+    } else {
+      setName("cheran");
+      setToggle(false);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>printing name {name}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="update name" onPress={clickHandler} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "pink",
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  buttonContainer: {
+    marginTop: 20,
   },
 });
