6aa07b561236a8819618ab6e05be56b42c1343fb
cheran <cherankumar.548@gmail.com>
2022-06-06 21:14:39 +0530

    todo app basic

--- App.js
+++ App.js
@@ -1,30 +1,35 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import Header from "./components/header";
+import TodoItem from "./components/TodoItem";
 
 export default function App() {
-  const [name, setName] = useState("cheran");
-  const [age, setAge] = useState("30");
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: 1 },
+    { text: "buy book", key: 2 },
+    { text: "sleep", key: 3 },
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key !== key);
+    });
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Enter Name:</Text>
-      {/* multiline prop inside TextInput */}
-      <TextInput
-        style={styles.input}
-        placeholder="Name.."
-        onChangeText={(val) => setName(val)}
-      />
-      <Text>Enter Age</Text>
-      <TextInput
-        keyboardType="numeric"
-        style={styles.input}
-        placeholder="age.."
-        onChangeText={(val) => setAge(val)}
-      />
-
-      <Text>name is {name}</Text>
-      <Text>age is {age}</Text>
+      <Header />
+      <View style={styles.content}>
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -33,14 +38,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
-  input: {
-    borderWidth: 1,
-    borderColor: "#777",
-    padding: 8,
-    margin: 10,
-    width: 200,
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
